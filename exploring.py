9e9901dc4f1abe55476ab6568094fd5cb8cd433f
Rajit Halder <rajithalder1698@gmail.com>
2025-07-02 10:36:58 -0500

    Preprocessing in the transformers library

--- exploring.py
+++ exploring.py
@@ -11,10 +11,18 @@ import os
 load_dotenv(override=True)
 hf_token = os.getenv("HF_API")
 
-login(hf_token, add_to_git_credential=False)
+# login(hf_token, add_to_git_credential=False)
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B", trust_remote_code=True, use_auth_token=hf_token)
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct", trust_remote_code=True, token=hf_token)
 
-encoded = tokenizer.encode("I Lost my job")
+system_msg = "You are a helpful assistant"
+user_msg = "What are black holes?"
 
-print(encoded)
+messages = [
+                {"role":"system","content":system_msg},
+                {"role":"user","content":user_msg}
+            ]
+
+prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generaation_prompt=True)
+
+print(prompt)
